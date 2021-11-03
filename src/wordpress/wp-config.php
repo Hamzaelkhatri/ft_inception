ae9403d8c3e51a58bc73a9074810c873aa8aedd1
Kali <kali@kali>
2021-11-03 15:23:43 -0400

    redis-cash

--- src/wordpress/wp-config.php
+++ src/wordpress/wp-config.php
@@ -55,6 +55,9 @@ define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
 define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
 define( 'NONCE_SALT',       'put your unique phrase here' );
 
+
+define( 'WP_CACHE_KEY_SALT', 'hello wordl' );
+
 /**#@-*/
 
 /**
